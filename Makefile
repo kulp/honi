ff09a052297c7097ab9f6c7ff5afaf2c4ec23d59
Darren Kulp <darren@kulp.ch>
2009-09-19 12:20:41 -0500

    API refactoring, options for printing, deps.
    Added .d dependency generation.
    Added 'clean' target.
    Changed hd_dump() and hd_yaml() interfaces to not require a state object
        and to be consistent with each other
    Added pretty-printing option.

--- Makefile
+++ Makefile
@@ -3,7 +3,25 @@ CFLAGS  += -std=c99 -W -Wall -Werror -Wextra -pedantic-errors $(addprefix -D,$(D
 # for debugging only
 CFLAGS  += -g3 -Wno-unused
 LDLIBS  += -lsyck
+CFILES  += $(wildcard *.c)
 
 all: hd2yaml
 
 hd2yaml: hd2yaml.o hd_parser.o filestore.o
+
+CLEANFILES += hd2yaml *.[od]
+
+.PHONY: clean
+clean:
+	-$(RM) -r $(CLEANFILES)
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(CFILES:.c=.d)
+endif
+
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) $(CFLAGS) -MM -MG -MF $@.$$$$ $<; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
--- hd_parser.h
+++ hd_parser.h
@@ -1,19 +1,69 @@
 #ifndef HD_PARSER_H_
 #define HD_PARSER_H_
 
+#include <stdbool.h>
 #include <stddef.h>
 
 struct hd_parser_state;
 
+#define HD_PRINT_PRETTY 1
+
+/**
+ * Called to initialize an opaque HoNData parser state.
+ *
+ * @param state a pointer to a state pointer
+ *
+ * @return zero on success, non-zero on undifferentiated failure
+ */
 int hd_init(struct hd_parser_state **state);
+
+/**
+ * Called to finalize an opaque HoNData parser state. Frees all internal data
+ * associated with the state, but not any data returned by hd_parse().
+ *
+ * @param state a pointer to a state pointer
+ *
+ * @return zero on success, non-zero on undifferentiated failure.
+ */
 int hd_fini(struct hd_parser_state **state);
-int hd_get_out_fd(struct hd_parser_state *state);
-int hd_set_out_fd(struct hd_parser_state *state, int out_fd);
+
+/** @defgroup getset Getters and setters for state internals */
+/** @{ */
 void* hd_get_userdata(struct hd_parser_state *state);
 int hd_set_userdata(struct hd_parser_state *state, void *data);
+/** @} */
+
+/**
+ * Parses the set-up store and returns the resulting tree.
+ *
+ * @param state the parser state
+ *
+ * @return a @c node or @c NULL on undifferentiated error
+ */
 struct node *hd_parse(struct hd_parser_state *state);
-int hd_yaml(const struct hd_parser_state *state, const struct node *node);
-int hd_dump(const struct node *node);
+
+/**
+ * Dumps a particular tree or subtree, as YAML, to a file descriptor @p fd.
+ *
+ * @param fd   the file descriptor to which to dump
+ * @param node the root of the (sub)tree to dump
+ * @param flags OR'ed flags that control the output
+ *
+ * @return zero on success, non-zero on undifferentiated error
+ */
+int hd_yaml(int fd, const struct node *node, int flags);
+
+/**
+ * Dumps a particular tree or subtree, in the native format, to the file
+ * descriptor @p fd.
+ *
+ * @param fd    the file descriptor to which to dump
+ * @param node  the root of the (sub)tree to dump
+ * @param flags OR'ed flags that control the output
+ *
+ * @return zero on success, non-zero on undifferentiated error
+ */
+int hd_dump(int fd, const struct node *node, int flags);
 
 void* hd_alloc(size_t size);
 void hd_free(struct node* ptr);
